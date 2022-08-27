d4d7b405f94240c4be9468c0d44bc14574e4a535
“jms” <jstapleton536@gmail.com>
2022-08-27 13:52:37 -0400

    dynmaic creation of html elements with data

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,14 +1,42 @@
-// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}&units=imperial
+//https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
 
+//http://api.openweathermap.org/data/2.5/forecast/daily?q=london&units=metric&APPID=value&cnt=7
+var displayElement = document.querySelector("#display");
 var appKey = "b305ebfd8dd85ed3fe2ac8280f681733";
 var city = "Asbury Park";
-var requestURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+appKey;
+var requestURLforUV ="https://api.openweathermap.org/data/2.5/uvi?q=portland&appid="+appKey+"&lat="+lat+"&lon="+lon;
+var requestURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&exclude=current,minutely,hourly,alerts&appid="+appKey
++"&units=imperial";
 console.log(requestURL);
+var lat;
+var lon;
 
+
+//current weather
 fetch(requestURL)
 .then(function (response) {
     return response.json();
 })
 .then(function (data) {
     console.log(data);
+    var temp = document.createElement("li");
+    temp.textContent = "Temperature: "+data.main.temp + " Degrees F";
+    displayElement.appendChild(temp);
+    var wind = document.createElement("li");
+    wind.textContent = "Wind: "+data.wind.speed + " MPH";
+    displayElement.appendChild(wind);
+    lat = data.coord.lat;
+    lon = data.coord.lon;
+    console.log("lat: " + lat + " lon: " + lon);
+
 });
+
+fetch(requestURLforUV)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    })
+
