4f607baf380ee00c0671e33e56cb1496c04cdfa8
“jms” <jstapleton536@gmail.com>
2022-09-03 13:12:53 -0400

    working search history

--- assets/js/scripts copy.js
+++ assets/js/scripts copy.js
@@ -1,47 +1,48 @@
-// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}&units=imperial
-//https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-
-//http://api.openweathermap.org/data/2.5/forecast/daily?q=london&units=metric&APPID=value&cnt=7
 var displayElement = document.querySelector("#display");
+var historyListElement = document.querySelector("#history-list");
 var appKey = "b305ebfd8dd85ed3fe2ac8280f681733";
 var city = "New York, NY";
-// var requestURLforUV ="https://api.openweathermap.org/data/2.5/uvi?q=portland&appid="+appKey+"&lat="+lat+"&lon="+lon;
-// var requestURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&exclude=current,minutely,hourly,alerts&appid="+appKey
-// +"&units=imperial";
+
 console.log("First URL");
+
+// URLS for fetch requests
 var requestURL = "https://api.openweathermap.org/geo/1.0/direct?q="+city+",&appid="+appKey;
 var requestURLDaily = "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid="+appKey;
 console.log(requestURL);
-var lat;
-var lon;
+var lat; //latitude from first fetch
+var lon; //longitude from second fetch (for 5-day forecast which can't be searched by city)
 var inputElement = document.querySelector("#city");
 var searchButton = document.querySelector("#search");
 
-var cityArray;
+// logic for local storage
+// Use an array to push search values to, which will then be set to storage (JSON Stringify)
+// If there are values in local storage, get and parse using JSON Parse
+var cityArray; 
 var citiesStored = localStorage.getItem("cities");
-if (!citiesStored) {
+if (!citiesStored) { // if there is nothing in local storage, we need an empty array
     cityArray = ["Test"];
 }
-else {
+else { //Otherwise we parse the local storage into an array
     cityArray=JSON.parse(citiesStored);
+    populateSearchHistory(cityArray);
 }
-console.log(cityArray);
+console.log(cityArray); // Check what's in the array
 
-
+// Search button
 searchButton.addEventListener("click", function () {
-    city = inputElement.value;
+    city = inputElement.value; // The city the user typed in. Should be added to search history
+    //Add city to the requestURL for the first fetch
     requestURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&exclude=current,minutely,hourly,alerts&appid="+appKey+"&units=imperial"
-    console.log("This should say howell " +city);
-    displayElement.innerHTML = "";
-    var displayHeader = document.createElement("h2");
+    displayElement.innerHTML = ""; //Clear out whatever was in the display
+    var displayHeader = document.createElement("h2"); //Create a header for the city name display
     displayHeader.textContent = city; // + moment().format("MMM Do, YYYY");
-    displayElement.appendChild(displayHeader);
-    weather();
+    displayElement.appendChild(displayHeader); //Add the header to the display element
+    weather(); 
     searchHistory(city);
 })
 
 
-//current weather
+//current weather: Fetch number 1
 function weather () {
 fetch(requestURL)
 .then(function (response) {
@@ -49,15 +50,19 @@ fetch(requestURL)
 })
 .then(function (data) {
     console.log(data);
+    //Get and Add the temperature to the display
     var temp = document.createElement("li");
     temp.textContent = "Temperature: "+data.main.temp + " Degrees F";
     displayElement.appendChild(temp);
+    //Get and add the wind speed to the display
     var wind = document.createElement("li");
     wind.textContent = "Wind: "+data.wind.speed + " MPH";
     displayElement.appendChild(wind);
+    //Get and add the humidity to the display
     var hum = document.createElement("li");
     hum.textContent = "Humidity: "+data.main.humidity;
     displayElement.appendChild(hum);
+    //Get the latitude and longitude for the second fetch
     lat = data.coord.lat;
     lon = data.coord.lon;
     console.log("lat: " + lat + " lon: " + lon);
@@ -76,36 +81,38 @@ fetch(requestURLDaily)
     })
     .then(function (data) {
         console.log(data);
+        //Create card for 5 days. For loop
+        //Get the daily info I need, plus the day (moment)
     })
-
 }
 }
 
 function searchHistory(city){
-
     pushCity = ""+city;
     console.log("Search history city: " +city);
     console.log("pushCity = " + pushCity);
     console.log("Is my array the null problem??? " +cityArray);
-    cityArray[0]=pushCity;
+    cityArray.unshift(pushCity);
     localStorage.setItem("cities",JSON.stringify (cityArray));
+    populateSearchHistory(cityArray);
 }
 
-function getCities() {
-    if (cityArray) {
+function populateSearchHistory(cityArray) {
+    historyListElement.innerHTML = ""; //clear current history
+    // for loop to append each element in array to the ul
+    //! Event delegation to make each list item clickable. Remember some hover effects and cursor
+    for (var i = 0; i<cityArray.length; i++) {
+    var listEl = document.createElement("li");
+    listEl.textContent = cityArray[i];
+    historyListElement.appendChild(listEl);
+}
+historyListElement.addEventListener("click", function (event) {
+    var cityClicked = event.target;
+    city = cityClicked.textContent;
+    console.log(city);
+    weather(); 
+})
 
-    cityArray = JSON.parse(localStorage.getItem("cities"));
-    
-    for (let index = 0; index < cityArray.length; index++) {
-        var listEl = document.createElement("li");
-        listEl.textContent = cityArray[index];
-        console.log("I don't know what this function does " +cityArray[index]);
-    }
 }
-    console.log(localStorage.getItem("cities"));
-    // var cityList = document.
-}
-if (cityArray) {
-    getCities();
-}
+
 
